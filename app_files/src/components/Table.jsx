ca7475c3f2e1ae25f1cd903db0c096fcf3857385
Dodilei <mauricioandrade2002@gmail.com>
2020-07-05 12:32:11 -0400

    Combine table components in one file 'Table.jsx'

--- app_files/src/components/Table.jsx
+++ app_files/src/components/Table.jsx
@@ -1,5 +1,63 @@
 import { Component } from "react";
 
+class TableCell extends Component {
+    constructor(props) {
+        this.data = props.data;
+        this.seq = props.seq;
+        this.state = {};
+        this.state.styleClasses = props.styleClasses;
+    };
+
+    render() {
+        return (
+            <td className={this.seq}>
+                <div className={this.state.styleClasses.join(" ")}>
+                    <span>
+                        {this.data}
+                    </span>
+                </div>
+            </td>
+        )
+    };
+};
+
+class TableRow extends Component {
+    constructor(props) {
+        this.title = props.title;
+        this.description = props.description;
+        this.state = {};
+        this.state.open = props.open;
+        this.state.styleClasses = props.styleClasses;
+
+        //save only modified rows
+        //memory improvement? will i need this?
+        this.cell_data = props.cell_data;
+        this.cells = [];
+        for (let i = 0; i < this.cell_data.length; i++) {
+            let ccell_data = this.cell_data[i];
+            this.cells.push(
+                <TableCell data={ccell_data.data}
+                seq={ccell_data.seq}
+                styleClasses={ccell_data.styleClasses}/>
+            );
+        };
+    };
+
+    render() {
+        //add this
+        if (!this.state.open) {
+            return (
+                <tr>
+                    {this.cells}
+                </tr>
+            );
+        } else {
+            1===1;
+        };
+            
+    };
+};
+
 class STable extends Component {
     constructor(props) {
         this.id = props.id;
