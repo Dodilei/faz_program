763eea8a65aed4edf056545bae7e7507cfba15d9
Dodilei <mauricioandrade2002@gmail.com>
2020-07-17 16:02:30 -0400

    Clean up front-end branch

--- app_files/src/App.jsx
+++ app_files/src/App.jsx
@@ -1,29 +1,10 @@
 import React from 'react';
-import Table from './components/Table';
-import {showOverflow} from './components/user_events/TableCellEvents';
-import SelWrapper from './components/SelectionWrapper'
 
-import argBinder from './util/argument_binder';
-
-import argBinder from './util/argument_binder';
-
-const child_process = require('child_process');
-let FILE = child_process.execSync("node -e 'console.log(")
 
 function App() {
   return (
-    <Table
-      id ='table'
-      userEvents ={{
-        row: {
-
-        },
-        cell: {
-          onClick: argBinder(showOverflow, SelWrapper)
-        }
-      }}
-      rowsData ={testObject}/>
+    <div>{"APP"}</div>
   );
 }
 
-export default App;
+export default App
