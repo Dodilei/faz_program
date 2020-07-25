85f8227ac3fb71d67789e79b5a286618db319438
Dodilei <mauricioandrade2002@gmail.com>
2020-07-25 15:58:05 -0400

    Make import_data.py methods executable from command-line

--- import_data.py
+++ import_data.py
@@ -2,14 +2,38 @@ import pandas as pd
 import os
 from events import Sale, Purchase
 
-FILE = ""
-SHEET_INDEX = 0
+def analyze(file, **kwargs):
+    sheet = pd.read_excel(
+        file,
+        header = None,
+        keep_default_na=False,
+        na_values="",
+        **kwargs
+    )
 
-DEFAULT_CONFIG = {
-    "header": None
-}
+    pass
+   
 
+if __name__ == "__main__":
+    import argparse
 
-sheet = pd.read_excel(file, header = 0, skiprows = 2, index_col = None,
-    usecols = "A:E", parse_dates = [0], keep_default_na=False)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "file",
+        help="excel file to import",
+    )
+    parser.add_argument(
+        "-a", "--analyze",
+        action="store_true",
+        help="Analyze sheet, try to acknowledge boundaries and dtypes"
+    )
+    parser.add_argument(
+        "-n", "--sheet_index",
+        help="zero-indexed sheet number",
+        default=0
+    )
 
+    args = parser.parse_args()
+
+    if args.analyze:
+        analyze(args.file, sheet_name=args.sheet_index)
