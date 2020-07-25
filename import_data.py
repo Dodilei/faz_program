fb50a0f562ecab3c46f4775658b8d72b6e53e5ff
Dodilei <mauricioandrade2002@gmail.com>
2020-07-25 16:00:05 -0400

    Start working on main sheet block identifying

--- import_data.py
+++ import_data.py
@@ -2,6 +2,33 @@ import pandas as pd
 import os
 from events import Sale, Purchase
 
+def find_blocks(array, value, return_all = True):
+    blocks = []
+
+    last = "undefined"
+    for index, item in enumerate(array):
+        current = (item == value)
+
+        if last == current:
+            pass
+
+        else:
+            if last != "undefined":
+                end = index-1
+                blocks.append(((start, end), btype))
+
+            start = index
+            btype = current
+        
+        last = current
+
+    blocks.append(((start, index), btype))
+    
+    if return_all:
+        return blocks
+    else:
+        return [block for block, btype in blocks if btype]
+
 def analyze(file, **kwargs):
     sheet = pd.read_excel(
         file,
@@ -11,7 +38,17 @@ def analyze(file, **kwargs):
         **kwargs
     )
 
-    pass
+    nas = sheet.apply(lambda r: r.dropna().empty, axis = 1)
+    sizes = sheet.apply(lambda r: r.dropna().size, axis = 1)
+    blocks = find_blocks(nas, False, return_all = False)
+
+    for b in blocks:
+        start, end = b
+        if end - start < 1:
+            blocks.remove(b)
+        elif sizes[start] < sizes[start:end+1].max()*0.8:
+            blocks.remove(b)
+
    
 
 if __name__ == "__main__":
